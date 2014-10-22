b00416621cfe069fd0fc2f0a071a11fbf5d7a2e1
root <root@icinga.konga.com>
2014-10-22 12:28:04 +0100

    Structure Update

---
+++ newrelic-ng/metadata.rb
@@ -0,0 +1,14 @@
+name             'newrelic-ng'
+maintainer       'Chris Aumann'
+maintainer_email 'me@chr4.org'
+license          'GNU Public License 3.0'
+description      'Installs/Configures newrelic monitoring'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.1'
+depends          'apt'
+depends          'build-essential'
+depends          'python'
+
+%w{ ubuntu debian redhat centos amazon suse scientific }.each do |os|
+  supports os
+end
